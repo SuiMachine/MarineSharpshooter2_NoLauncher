9dbbb2c6c8dfc40459343ff5fd72de2559311578
Sui <link_matrix1@o2.pl>
2015-02-13 00:26:46 +0100

    Initial Release

---
+++ NoLauncher/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoLauncher
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main(string[] args)
+        {
+            string fullargs="-windowtitle \"Marine Sharpshooter 2\" -rez Engine.rez -rez game.rez +DisableMusic 0 +DisableSound 0 +DisableMovies 0 +DisableJoystick 1 +EnableTripBuf 0 +DisableHardwareCursor 0";   //required to get the game running
+            int length;
+            length = args.GetLength(0);
+
+            for (int i = 0; i!=length; i++)     //a lazy way of handling additional parameters
+            {
+                fullargs = fullargs + " "+ args[i];
+            }
+            try
+            {
+                Process firstProc = new Process();
+                firstProc.StartInfo.FileName = "Lithtech.exe";
+                firstProc.StartInfo.Arguments = fullargs;
+                firstProc.EnableRaisingEvents = true;
+
+                firstProc.Start();
+                firstProc.WaitForExit();   
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred!!!: " + ex.Message);
+                return;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+﻿Marine Sharpshooter 2 No Launcher
+=====================
+
+This is a simple program, that is to be used as a replacement for Marine Sharpshooter 2 Launcher.
+While the launcher itself isn't broken, it's absolutely useless and it doesn't pass the launch parameters from Steam,
+hence the reason for my replacement.
+
+Features
+--------
+  * Launches the game instantly.
+  * Passes launch parameters from Steam to a LithTech.exe, allowing to easily launch the game in Windowed mode etc.
+
+Requirements
+------------
+
+  * Marine Sharpshooter 2
+  * .NET Framework 4
+ 
+Install
+-------
+Download the executable from the [releases page](https://github.com/SuiMachine/MarineSharpshooter2_NoLauncher/releases) and use it to replace the original launcher.
+Make sure to add an exception in your antivirus (if needed) and check it as trusted. If you're afraid of it doing something to your PC, look at the source-code :)
+
+Credits
+-------
+  * [SuicideMachine](http://twitch.tv/suicidemachine)
